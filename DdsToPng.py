186a6cf78a0e0cb8c6a3dd3708c25834e00efdc2
JakobPCoder <j4712@web.de>
2022-11-14 00:44:28 +0100

    fixed script content

---
+++ DdsToPng.py
@@ -0,0 +1,24 @@
+import os
+import cv2
+import numpy as np
+import pathlib
+from wand.image import Image
+
+INPUT_FILE_FORMAT = ".dds"
+OUTPUT_FILE_FORMAT = ".png"
+
+def processImage(path, entry): 
+    newPath = path.parent / (path.stem + OUTPUT_FILE_FORMAT)   
+    cv2.imwrite(str(newPath), cv2.cvtColor(np.array(Image(filename = path)), cv2.COLOR_BGRA2RGBA))
+    os.remove(entry)
+    print(newPath)  
+    return
+
+basePath = os.path.dirname(os.path.realpath(__file__))
+with os.scandir(basePath) as dir:
+    for entry in dir:
+        if entry.is_file():
+            path = pathlib.Path(entry)       
+            if path.suffix == INPUT_FILE_FORMAT:                           
+                processImage(path, entry)
+                
